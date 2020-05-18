4747348592993133ca2de2bc34b28aa77c8e2fce
Boomshakal <31717950+Boomshakal@users.noreply.github.com>
2020-05-18 17:22:45 +0800

    Create test.py

---
+++ 正则re/test.py
@@ -0,0 +1,9 @@
+import re
+str = """ lr<lr@ikahe.net>
+xuguochun<xuguochun@ikahe.net>,   lxw<lxw@ikahe.net>,   zhangcheng<zhangcheng@ikahe.net>,   yft<yft@ikahe.net>,   sdj<sdj@ikahe.net>,   xudewei<xudewei@ikahe.net>,   maxinhong<maxinhong@ikahe.net>,   姜连洋<jianglianyang@ikahe.net>,   cg<cg@ikahe.net>,   gongsaikang<gongsaikang@ikahe.net>,   马臻<mazhen@ikahe.net>,   hhmeg<hhmeg@megmeet.com>,  刘勇<liuyong@ikahe.net>,   邱勇<qiuyong@ikahe.net>,   wanglin<wanglin@ikahe.net>,   zhumaoqun<zhumaoqun@ikahe.net>,   quxuehui<quxuehui@ikahe.net>,   wxy<wxy@ikahe.net>,   zhouxiaobo<zhouxiaobo@ikahe.net>,   huangcao<huangcao@ikahe.net>,   wj<wj@ikahe.net>,   wangchuanyong<wangchuanyong@ikahe.net>,      黄强<huangqiang@ikahe.net>,   yinqinai@ikahe.net<yinqinai@ikahe.net>， cwj<cwj@ikahe.net,xgq<xgq@ikahe.net>，解根雄<jiegenxiong@ikahe.net>,jiangshaoting<jiangshaoting@ikahe.net>
+yjg<yjg@ikahe.net>，zhangcheng<zhangcheng@ikahe.net>
+"""
+
+it = re.finditer(r"[a-zA-Z0-9_-]+@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)",str)
+for match in it:
+    print (match.group(0))
